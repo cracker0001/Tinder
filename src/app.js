2984ec7c7b42a45393eaeeb71177d5c14fdd7d53
premraj0001 <prem.raj@moonrider.ai>
2025-09-21 11:37:58 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+const PORT = 5000;
+
+app.get('/',(req,res)=>{
+    res.send("hello server")
+})
+app.listen(PORT,()=>{
+    console.log(`server is ruuning on ${PORT}`)
+})
